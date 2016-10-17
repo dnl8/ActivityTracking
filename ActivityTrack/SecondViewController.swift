ddd5aff58d7d55943eca0b462749859a071e6f64
dnl8 <Daniel@Daniels-MacBook-Pro.local>
2016-10-17 10:30:53 +0200

    Initial Commit

---
+++ ActivityTrack/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ActivityTrack
+//
+//  Created by Daniel Holzmann on 17.10.16.
+//  Copyright © 2016 Daniel Holzmann. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
