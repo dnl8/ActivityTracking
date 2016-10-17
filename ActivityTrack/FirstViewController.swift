c30a9edd7b89bb527eae6908db214a6600eb6891
dnl8 <Daniel@Daniels-MacBook-Pro.local>
2016-10-17 10:50:10 +0200

    Add MotionManager

--- ActivityTrack/FirstViewController.swift
+++ ActivityTrack/FirstViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import CoreMotion
 
 class FirstViewController: UIViewController {
-
+    var motionManager: CMMotionManager!
     override func viewDidLoad() {
         super.viewDidLoad()
+        initMotionManager()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initMotionManager(){
+        motionManager = CMMotionManager()
+        motionManager.gyroUpdateInterval = 5
+        if motionManager.isDeviceMotionAvailable {
+            //do something interesting
+        }
+        
+        motionManager.startDeviceMotionUpdates(
+            to: OperationQueue.current!, withHandler: {
+                (deviceMotion, error) -> Void in
+                
+                if(error == nil) {
+                    self.handleDeviceMotionUpdate(motion: deviceMotion!)
+                } else {
+                    //handle the error
+                }
+        })
+        
+    }
+    
+    func handleDeviceMotionUpdate(motion:CMDeviceMotion){
+        // print(CGFloat(motion.userAcceleration.x))
+        let sum = sqrt(motion.userAcceleration.x * motion.userAcceleration.x + motion.userAcceleration.y * motion.userAcceleration.y + motion.userAcceleration.z * motion.userAcceleration.z)
+        print(sum)
+    
     }
 
 
